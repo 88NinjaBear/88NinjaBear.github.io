7f93d35366acb5826eae96775fa8a8f1773b82d6
88NinjaBear <zhudi19881008@gmail.com>
2025-02-03 19:18:01 -0800

    Site updated: 2025-02-03 19:18:01

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/03/My-first-article/","2025/02/02/hello-world/"];function toRandomPost(){
+var posts=["2025/02/02/hello-world/","2025/02/03/My-first-article/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
