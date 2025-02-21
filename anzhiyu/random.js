3e9d22e4a6470ed13d153bc28634bbcd3e71aab1
88NinjaBear <76588606+88NinjaBear@users.noreply.github.com>
2025-02-21 11:57:44 -0800

    Site updated: 2025-02-21 11:57:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/02/hello-world/","2025/02/03/My-first-article/"];function toRandomPost(){
+var posts=["2025/02/21/binary-search-review/","2025/02/02/hello-world/","2025/02/03/my-first-article/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
